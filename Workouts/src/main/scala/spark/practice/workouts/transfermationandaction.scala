fae34856ebffc9ab7bc826315327c39a38ab3a7c
sivak <sivak@Shiva>
2017-02-02 23:04:37 -0500

    Added Examples of reducebyKey , groupby on pair RDDs

--- Workouts/src/main/scala/spark/practice/workouts/transfermationandaction.scala
+++ Workouts/src/main/scala/spark/practice/workouts/transfermationandaction.scala
@@ -4,18 +4,40 @@ import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
 import org.apache.spark.io
 
-
 object transfermationandaction {
-  def main(args:Array[String])={
-    flatvsmap()
+  def main(args: Array[String]) = {
+    //flatvsmap()
+    workingwithKeyValuePairs()
+
   }
-  def flatvsmap()={
-    val conf= new SparkConf().setAppName("transfermationandaction").setMaster("local")
-    val sc= new SparkContext(conf)
-//    val rdd= sc.textFile("panda.txt")
-    val rdd= sc.parallelize(List("coffee panda","happy panda","happiest panda party"))
-    rdd.flatMap(x=>x.split(" ")).saveAsTextFile("flatmapresults")
-    rdd.map(y=>(y,1)).saveAsTextFile("mapresults")
+  def flatvsmap() = {
+    val conf = new SparkConf().setAppName("transfermationandaction").setMaster("local")
+    val sc = new SparkContext(conf)
+    //    val rdd= sc.textFile("panda.txt")
+    val rdd = sc.parallelize(List("coffee panda", "happy panda", "happiest panda party"))
+    rdd.flatMap(x => x.split(" ")).saveAsTextFile("flatmapresults")
+    rdd.map(y => (y, 1)).saveAsTextFile("mapresults")
+
+  }
+  def workingwithKeyValuePairs() = {
+    val conf = new SparkConf().setAppName("transfermationandaction").setMaster("local")
+    val sc = new SparkContext(conf)
+    val lines = sc.textFile("map.txt")
+    //lines.saveAsTextFile("map.rdd.txt")
     
-    }
- }
+    //Converting string input into Int. 
+    //x(0).toString().toInt - To understand why do I convert input string values to a string then to a Int? 
+    // http://stackoverflow.com/questions/42016117/why-spark-map-produces-different-result-as-rdd-when-input-element-converted-to-i 
+    val pairRDD = lines.map(x => (x(0).toString().toInt, x(1).toString().toInt))
+    pairRDD.saveAsTextFile("map.pairrdd.txt")
+    
+    //reduceByKey Function
+    val reducedByKeyRDD = pairRDD.reduceByKey((x, y) => x + y)
+    reducedByKeyRDD.saveAsTextFile("map.reduceByKey.txt")
+
+    //groupByKey Function
+    val groupByKey = pairRDD.groupByKey()
+    groupByKey.saveAsTextFile("map.groupByKey.txt")
+
+  }
+}
